9f14cd06ecf17889d135b3964417ea0ccfee87e6
Luis Felipe Bertel M <ellipe@gmail.com>
2020-02-15 15:53:17 -0500

    methods, computed and watchers

--- app.js
+++ app.js
@@ -4,18 +4,47 @@ new Vue({
   data() {
     return {
       name: 'Bitcoin',
+      symbol : 'BTC',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 10,
-      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+
+      price: 8400,
+      color: 'f4f4f4',
+
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },
         { day: 'Martes', value: 7900 },
         { day: 'Miercoles', value: 8200 },
         { day: 'Jueves', value: 9000 },
-        { day: 'Viernes', value: 9400 },
+        { day: 'Viernes', value: 12000 },
         { day: 'Sabado', value: 10000 },
         { day: 'Domingo', value: 10200 },
-    ]
+      ],
+
+      showPrices: false
+    }
+  },
+
+  methods: {
+    toggleShowPrices(){
+      this.showPrices = !this.showPrices
+
+      // Night mode
+      this.color = this.color.split('').reverse().join('')
+    }
+  },
+  watch: {
+    showPrices(newVal, oldVal){
+      console.log(newVal, oldVal)
+    }
+  },
+
+  computed: {
+    title(){
+      return `${this.name} - ${this.symbol}`
+    },
+    backgroundColor(){
+      return `#${this.color}`
     }
   }
 })
